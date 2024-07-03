5ab0f226c830d7acdc0a4a22053371fc6112ffc9
keshendaren <1053071197@qq.com>
2024-07-04 01:19:45 +0800

    客户端导出路径调整

--- bin/Release/导出配置_客户端.bat
+++ bin/Release/导出配置_客户端.bat
@@ -6,7 +6,7 @@ Excel2Json.exe
 
 
 echo '拷贝Json到客户端'
-xcopy CJson\*.json ..\\..\\G01Client\\Assets\\Resources\\Dynamic\\TextAsset\\Config /s /y /h /r
+xcopy CJson\*.json ..\\..\\G01Client\\Assets\\Resources\\Dynamic\\TextAssets\\Config /s /y /h /r
 
 echo '拷贝配置定义C#文件d到客户端'
 xcopy CDef\*.cs ..\\..\\G01Client\\Assets\\Scripts\\Service\\Config /s /y /h /r
